71aa72bfdb8af6c6d06985bb8a48ebd0636d0156
Jon Fei <jhfernan@mail.usf.edu>
2021-03-30 16:29:56 +0800

    Added more functions and tested code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import random
 
 # Psuedo Code
 
@@ -12,13 +12,31 @@ def get_users_sign():
 
 # Make sure they used a legal sign
 def validate_input(response):
-	if response == "R" || response == "P" || response == "S":
-		return true
+	if response == "R" or response == "P" or response == "S":
+		return True
 	else:
 		print('You did not use a proper letter')
-		return false
-
+		return False
 
 # Figure who won or if it is a tie
+def determine_winner(res, comp_ai):
+	if res == comp_ai:
+		return "tie"
+	elif (res == "R" and comp_ai == "S") or (res == "S" and comp_ai == "P") or (res == "P" and comp_ai == "R"):
+		return "win"
+	else:
+		return "loss"
 
 # Tell the user what happened
+def print_win_screen(result):
+	if result == "win":
+		print("Good job, you won!")
+	elif result == "tie":
+		print("Close, but this round is a draw!")
+	else:
+		print("You lose because your bad at luck!")
+
+# Pick a random choice for the computer
+def computer_picks_an_answer():
+	choices = ["R", "P", "S"]
+	return random.choice(choices)
--- notes.py
+++ notes.py
@@ -1,1 +1,19 @@
+import random
+
 # This is for testing purposes
+
+# "R" and "R"
+# "R" and "S"
+# "R" and "P"
+
+# "P" and "R"
+# "P" and "S"
+# "P" and "P"
+
+# "S" and "R"
+# "S" and "S"
+# "S" and "P"
+
+school_choices = ["USF", "UCF", "Harvard", "USC", "USC (The one in California)"]
+
+print(random.choice(school_choices))

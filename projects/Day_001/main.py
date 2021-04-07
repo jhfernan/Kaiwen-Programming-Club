71f7c2a61bd1a0b4065e4cbcb7d14f0bb4343304
Jon Fei <jhfernan@mail.usf.edu>
2021-04-07 08:44:37 +0800

    Added a projects folder and day one project

---
+++ projects/Day_001/main.py
@@ -0,0 +1,6 @@
+# The first project should simply get them to take the input and have them return output saying something simple about
+# the data it got
+
+user_input = input('What is your first name?\n>>> ')
+
+print('Your name is {} characters long.'.format(len(user_input)))

4e7ba79f5fe59961689285ef80c25d0cb707bf82
Jon Fei <jhfernan@mail.usf.edu>
2021-04-14 08:45:39 +0800

    Updated and finalized game

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ def get_users_sign():
 	my_prompt += 'Press "R" for rock\n'
 	my_prompt += 'Press "P" for paper\n'
 	my_prompt += 'Press "S" for scissors\n'
+	my_prompt += 'Press "Q" to quit\n'
 	return input(my_prompt + '>>')
 
 # Make sure they used a legal sign
-def validate_input(response):
-	if response == "R" or response == "P" or response == "S":
+def validate_input(res):
+	if res == "R" or res == "P" or res == "S":
 		return True
 	else:
-		print('You did not use a proper letter')
+		print('You did not use a proper letter\n\n')
 		return False
 
 # Figure who won or if it is a tie
@@ -36,7 +37,29 @@ def print_win_screen(result):
 	else:
 		print("You lose because your bad at luck!")
 
+def print_choices(res, comp_res):
+	print("\n\tYou chose {}, and the computer chose {}".format(res, comp_res))
+
 # Pick a random choice for the computer
 def computer_picks_an_answer():
 	choices = ["R", "P", "S"]
 	return random.choice(choices)
+
+
+# Program start
+user_input = ""
+
+while True:
+	user_input = get_users_sign()
+	user_input = user_input.upper()
+	if user_input == 'Q':
+		break
+	if validate_input(user_input):
+		computers_answer = computer_picks_an_answer()
+		game_result = determine_winner(user_input, computers_answer)
+		print_win_screen(game_result)
+		print_choices(user_input, computers_answer)
+		if game_result == "tie":
+			pass
+		else:
+			break
